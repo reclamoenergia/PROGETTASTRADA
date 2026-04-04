f5108f7e445f80287a72d19dba25254a16d65d91
Gabriele <gabrieleconversano@hotmail.com>
2026-04-04 15:41:23 +0200

    Delete road_designer_plugin directory

--- road_designer_plugin/__init__.py
+++
@@ -1,7 +0,0 @@
-"""QGIS entrypoint for Road Designer Plugin."""
-
-
-def classFactory(iface):
-    from .main_plugin import RoadDesignerPlugin
-
-    return RoadDesignerPlugin(iface)
--- road_designer_plugin/core/terrain_sampler.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from .raster_terrain_provider import RasterTerrainProvider
-
-
-class TerrainSampler(RasterTerrainProvider):
-    """Backward-compatible alias to keep existing workflow unchanged."""
-
-    pass
